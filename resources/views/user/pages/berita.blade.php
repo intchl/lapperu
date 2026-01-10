6093d7d85a15f44bf5cfa43e7b9de7197ab26410
atha-nasya <nasyadina26@gmail.com>
2026-01-10 10:16:16 +0700

    add berita user

--- resources/views/user/pages/berita.blade.php
+++ resources/views/user/pages/berita.blade.php
@@ -1,4 +1,4 @@
-@extends('user.layout.master')
+@extends('user.layout.master') 
 
 @section('pages_name')
 Berita Kami
@@ -7,57 +7,56 @@ Berita Kami
 @section('content')
 <div class="container-fluid features overflow-hidden py-5">
     <div class="container">
-        <div class="section-title text-center wow fadeInUp" data-wow-delay="0.1s" style="margin-bottom: 50px;">
-                            <div class="sub-style">
-                                <h5 class="sub-title text-primary px-3">Berita</h5>
-                            </div>
-                            <h1 class="display-5 mb-4">Berita & Info Terkini</h1>
-                </div>
-        <div class="row g-4 justify-content-center text-center">
-            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="feature-item text-center p-4">
-                    <div class="feature-icon mb-4">
-                        <img src="img/berita1.jpeg" class="img-fluid" alt="Berita Lumajang">
-                    </div>
-                    <div class="feature-content d-flex flex-column">
-                        <h6 class="mb-3">LAPAS KELAS IIB LUMAJANG HASILKAN 50 TELUR AYAM PER HARI DARI PROGRAM PEMBINAAN PETERNAKAN</h6>
-                            <a class="btn btn-secondary rounded-pill" href="/berita-detail">Baca selengkapnya <i class="fas fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
+
+        <div class="section-title text-center mb-5">
+            <div class="sub-style">
+                <h5 class="sub-title text-primary px-3">Berita</h5>
             </div>
-            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="feature-item text-center p-4">
-                    <div class="feature-icon mb-4">
-                        <img src="img/berita1.jpeg" class="img-fluid" alt="Berita Lumajang">
-                    </div>
-                    <div class="feature-content d-flex flex-column">
-                        <h6 class="mb-3">LAPAS KELAS IIB LUMAJANG HASILKAN 50 TELUR AYAM PER HARI DARI PROGRAM PEMBINAAN PETERNAKAN</h6>
-                        <a class="btn btn-secondary rounded-pill" href="/berita-detail">Baca selengkapnya <i class="fas fa-arrow-right ms-2"></i></a>
+            <h1 class="display-5 mb-4">Berita & Info Terkini</h1>
+        </div>
+
+        <div class="row g-4">
+
+            @forelse ($berita as $item)
+                <div class="col-md-6 col-lg-4 col-xl-3">
+                    <div class="feature-item text-center p-4 h-100">
+
+                        <div class="feature-icon mb-4">
+                            @if ($item->foto)
+                                <img src="{{ asset('images/berita/' . $item->foto) }}" 
+                                     class="img-fluid" 
+                                     alt="{{ $item->judul }}">
+                            @else
+                                <img src="{{ asset('img/default-berita.jpg') }}" 
+                                     class="img-fluid" 
+                                     alt="Berita">
+                            @endif
+                        </div>
+
+                        <div class="feature-content d-flex flex-column">
+                            <h6 class="mb-3">
+                                {{ Str::limit($item->judul, 80) }}
+                            </h6>
+
+                            <small class="text-muted mb-3">
+                                {{ \Carbon\Carbon::parse($item->tanggal)->format('d M Y') }}
+                            </small>
+
+                            <a class="btn btn-secondary rounded-pill mt-auto"
+                               href="{{ url('/berita-detail/' . $item->id) }}">
+                                Baca selengkapnya
+                                <i class="fas fa-arrow-right ms-2"></i>
+                            </a>
+                        </div>
+
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="feature-item text-center p-4">
-                    <div class="feature-icon mb-4">
-                        <img src="img/berita1.jpeg" class="img-fluid" alt="Berita Lumajang">
-                    </div>
-                    <div class="feature-content d-flex flex-column">
-                        <h6 class="mb-3">LAPAS KELAS IIB LUMAJANG HASILKAN 50 TELUR AYAM PER HARI DARI PROGRAM PEMBINAAN PETERNAKAN</h6>
-                        <a class="btn btn-secondary rounded-pill" href="#">Baca selengkapnya <i class="fas fa-arrow-right ms-2"></i></a>
-                    </div>
+            @empty
+                <div class="col-12 text-center">
+                    <p>Belum ada berita.</p>
                 </div>
-            </div>
-            <div class="col-md-6 col-lg-6 col-xl-3 wow fadeInUp" data-wow-delay="0.1s">
-                <div class="feature-item text-center p-4">
-                    <div class="feature-icon mb-4">
-                        <img src="img/berita1.jpeg" class="img-fluid" alt="Berita Lumajang">
-                    </div>
-                    <div class="feature-content d-flex flex-column">
-                        <h6 class="mb-3">LAPAS KELAS IIB LUMAJANG HASILKAN 50 TELUR AYAM PER HARI DARI PROGRAM PEMBINAAN PETERNAKAN</h6>
-                        <a class="btn btn-secondary rounded-pill" href="#">Baca selengkapnya <i class="fas fa-arrow-right ms-2"></i></a>
-                    </div>
-                </div>
-            </div>
+            @endforelse
+
         </div>
     </div>
 </div>
